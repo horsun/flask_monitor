b1824a01ddd07544089698af951cebc56da52bc4
jinhao <im7h1995@hotmail.com>
2018-11-07 13:37:34 +0800

    help text

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
+# init app
 app = Flask(__name__)
-api = Api(app)
+app.config['SECRET_KEY'] = 'secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@127.0.0.1/monitor'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# init others ,like url database
 db = SQLAlchemy(app)
+api = Api(app)
 
 import monitor.apps.monitor_app.urls
 import monitor.apps.monitor_app.models
 
 db.create_all()
-# db.drop_all()  # 删除表
